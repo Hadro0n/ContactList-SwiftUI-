50c332f5a2212a41dd761a9cce209eb4c62c4f56
Hadro0n <v.w.yakunin@gmail.com>
2025-04-08 14:41:49 +0300

    Initial Commit

---
+++ ContactList(SwiftUI)/ContactList_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SwiftUI_App.swift
+//  ContactList(SwiftUI)
+//
+//  Created by Владислав Якунин on 08.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
